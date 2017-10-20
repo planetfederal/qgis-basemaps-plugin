e6025327746871a59a57cf1f1930d7ce324a8e66
Alessandro Pasotti <apasotti@boundlessgeo.com>
2017-10-20 16:01:03 +0200

    Disable oauth2 check

--- boundlessbasemaps/plugin.py
+++ boundlessbasemaps/plugin.py
@@ -66,8 +66,8 @@ class Basemaps:
     def setup(self, username=None, password=None):
         """Configuration wizard"""
         # Preliminary check:
-        if not utils.bcs_supported() and not os.environ.get('TRAVIS'):
-            return QMessageBox.warning(None, self.tr("Basemaps error"), self.tr("Your QGIS installation does not meet the minimum requirements to run this plugin. Please check if the OAUth2 authentication plugin is installed and have a look to the documentation for further information."))
+        #if not utils.bcs_supported():
+        #    return QMessageBox.warning(None, self.tr("Basemaps error"), self.tr("Your QGIS installation does not meet the minimum requirements to run this plugin. Please check if the OAUth2 authentication plugin is installed and have a look to the documentation for further information."))
         from gui.setupwizard import SetupWizard
         settings = {
             "maps_uri": pluginSetting('maps_uri'),
